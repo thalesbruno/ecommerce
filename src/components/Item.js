635dbffd60f2e65f650ff1bd6d8565aafd2f16d6
@thalesbruno <thalesbrno@gmail.com>
2021-10-06 11:10:04 -0300

    feat(cart): create cart page and all related components and funcionalities

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,6 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -10,41 +10,66 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import Rating from '@mui/material/Rating';
+import { CartContext } from '../contexts/CartContext';
 
-const Item = ({ product }) => (
-  <Card>
-    <CardMedia
-      sx={{ marginTop: '8px' }}
-      component="img"
-      image={require(`../assets/${product.image}`).default}
-    />
-    <CardContent>
-      <Typography>
-        {product.name}
-      </Typography>
-      <Typography variant="h6">
-        R$
-        {' '}
-        {product.price.toFixed(2)}
-      </Typography>
-      <Rating size="small" readOnly value={(product.score / 400) * 5} />
-    </CardContent>
-    <CardActions sx={{ justifyContent: 'center' }}>
-      <Button
-        startIcon={<AddShoppingCartIcon />}
-        variant="contained"
-      >
-        Adicionar ao carrinho
-      </Button>
-    </CardActions>
-  </Card>
-);
+const Item = ({ product }) => {
+  const [cart, setCart] = useContext(CartContext);
+
+  const handleClick = () => {
+    const productList = cart.products;
+    const idx = productList.findIndex((p) => p.id === product.id);
+    if (idx !== -1) {
+      productList[idx].count += 1;
+      setCart({
+        ...cart,
+        products: [...productList],
+      });
+    } else {
+      setCart({
+        ...cart,
+        products: [...cart.products, { ...product, count: 1 }],
+      });
+    }
+  };
+
+  return (
+    <Card>
+      <CardMedia
+        sx={{ marginTop: '8px' }}
+        component="img"
+        image={require(`../assets/${product.image}`).default}
+      />
+      <CardContent>
+        <Typography>
+          {product.name}
+        </Typography>
+        <Typography variant="h6">
+          R$
+          {' '}
+          {product.price.toFixed(2)}
+        </Typography>
+        <Rating size="small" readOnly value={(product.score / 400) * 5} />
+      </CardContent>
+      <CardActions sx={{ justifyContent: 'center' }}>
+        <Button
+          startIcon={<AddShoppingCartIcon />}
+          variant="contained"
+          onClick={handleClick}
+        >
+          Adicionar ao carrinho
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
 Item.propTypes = {
   product: PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string,
     price: PropTypes.number,
     score: PropTypes.number,
+    count: PropTypes.number,
     image: PropTypes.string,
   }).isRequired,
 };
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,17 +1,43 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState({
-    shipping: 0.0,
     products: [],
+    shipping: 0.0,
     subtotal: 0.0,
     total: 0.0,
   });
+
+  const priceReducer = (
+    total, currentProduct,
+  ) => total + (currentProduct.price * currentProduct.count);
+
+  const shippingReducer = (
+    total, currentProduct,
+  ) => total + (currentProduct.count * 10.00);
+
+  useEffect(() => {
+    setCart({
+      ...cart,
+      subtotal: cart.products.length === 0 ? 0.0 : cart.products.reduce(priceReducer, 0),
+      shipping: cart.products.reduce(shippingReducer, 0),
+    });
+  }, [cart.products]);
+
+  useEffect(() => {
+    setCart({
+      ...cart,
+      total: cart.subtotal > 250 ? cart.subtotal : cart.subtotal + cart.shipping,
+    });
+  }, [cart.subtotal]);
+
+  // useEffect(() => console.log(cart));
+
   return (
-    <CartContext.Provider values={[cart, setCart]}>
+    <CartContext.Provider value={[cart, setCart]}>
       { children }
     </CartContext.Provider>
   );
